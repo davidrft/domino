5112606e337ceba2839879727fbdf1bb60e2580f
David Riff <davidriff@outlook.com>
2016-05-25 12:46:57 -0300

    funcao atribuirpecas

--- main.c
+++ main.c
@@ -2,12 +2,29 @@
 #include <stdlib.h>
 #include <conio.h>
 #include <windows.h>
+#include <math.h>
+#include <time.h>
+
+srand(time(NULL)*getpid());
 
 typedef struct {
     int a;
     int b;
 }peca;
 
+int random(int n) {
+    int f = (double)rand() / RAND_MAX * (n+1);
+    return f;
+}
+
+int escolha(int n, int m) {
+    int c;
+    do {
+        c = getch();
+        c = (int)(c - '0');
+    } while (c<n || c>m);
+    return c;
+}
 
 int menu() {
     int c;
@@ -21,10 +38,7 @@ int menu() {
     printf("\t\t1. Novo Jogo");
     printf("\n\t\t2. Continue Jogo");
     printf("\n\t\t3. Sair");
-    do {
-        c = getch();
-        c = (int)(c - '0');
-    } while (c<1 || c>3);
+    c = escolha(1, 3);
     if (c == 3) {
         exit(3);
     }
@@ -41,17 +55,17 @@ Lista* gerarpecas() {
             if (i=0 && j=0) {
                 v = lst_criar(p);
             } else {
-                lst_adicionar(p, v);
+                lst_add_fim(p, v);
             }
         }
     }
 }
 
-void printp (peca p) {
+void printp(peca p) {
     printf("(%d, %d)", p.a, p.b);
 }
 
-void printl (Lista* l) {
+void printl(Lista* l) {
     Lista* p = l->proximo;
     printf("   ");
     while (1) {
@@ -77,6 +91,23 @@ void printl (Lista* l) {
 
 }
 
+void atribuirpecas(lista* p, lista *v, int n) {
+    int i, aleat, tam;
+    tam = lst_tam(v);
+    for (i=0; i<n; i++) {
+        aleat = random(tam)
+        if (p==NULL) {
+            lst_criar(lst_get(v, aleat));
+            lst_del(v, aleat);
+            tam--;
+        }
+        else {
+            lst_add_end(lst_get(v, aleat));
+            lst_del(v, aleat);
+            tam--;
+        }
+    }
+}
 
 int main() {
     SetConsoleTitle("Projeto Métodos Computacionais - Domino Lista");
